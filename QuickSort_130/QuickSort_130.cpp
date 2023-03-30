f053a0c4ffdc10e45fcfed38247dc9da9560d2b3
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-03-30 14:59:54 +0700

    Add project files.

---
+++ QuickSort_130/QuickSort_130.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+using namespace std;
+
+//array of integers to hold values
+int arr[20];
+int cmp_count = 0; //number of comparisson
+int mov_count = 0;
+int n;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
